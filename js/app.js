260faf6858532b8e55d8ec68765db2abfdb05b46
Smithy01 <>
2021-08-08 13:15:24 -0700

    Use standard class format

--- js/app.js
+++ js/app.js
@@ -1,51 +1,52 @@
+class App {
+    constructor(canvasId) {    
+        var WIDTH, HEIGHT;
 
-function App(canvasId) {
-    var WIDTH, HEIGHT;
+        var animationLoopTimeInterval;
+        var game;
+        var canvas, context;
 
-    var animationLoopTimeInterval;
-    var game;
-    var canvas, context;
+        console.log("App object is being constructed.");
 
-    console.log("App object is being constructed.");
+        function Main() {
+            SetupCanvas();
 
-    function Main() {
-        SetupCanvas();
+            game = new Game(WIDTH, HEIGHT);
 
-        game = new Game(WIDTH, HEIGHT);
+            animationLoopTimeInterval = setInterval(AnimationLoop, 50);
+        }
+        
+        function SetupCanvas() {
+            canvas = document.getElementById(canvasId);
 
-        animationLoopTimeInterval = setInterval(AnimationLoop, 50);
-    }
-    
-    function SetupCanvas() {
-        canvas = document.getElementById(canvasId);
+            if (canvas && canvas.getContext) {
+                context = canvas.getContext('2d');
+                console.log('Initialized canvas');
+            }
 
-        if (canvas && canvas.getContext) {
-            context = canvas.getContext('2d');
-            console.log('Initialized canvas');
+            window.addEventListener('resize', ResizeCanvas, true);
+            ResizeCanvas();
         }
 
-        window.addEventListener('resize', ResizeCanvas, true);
-        ResizeCanvas();
+        function ResizeCanvas() {
+            WIDTH = window.innerWidth;
+            HEIGHT = window.innerHeight;
+
+            canvas.width = WIDTH;
+            canvas.height = HEIGHT;
+        }
+
+        function AnimationLoop() {
+            context.fillStyle = '#f8fbff';
+            context.fillRect(0, 0, WIDTH, HEIGHT);
+
+            game.Render(context, WIDTH, HEIGHT);
+        }
+        
+        Main();
+
+        game.Start();
     }
-
-    function ResizeCanvas() {
-        WIDTH = window.innerWidth;
-        HEIGHT = window.innerHeight;
-
-        canvas.width = WIDTH;
-        canvas.height = HEIGHT;
-    }
-
-    function AnimationLoop() {
-        context.fillStyle = '#f8fbff';
-        context.fillRect(0, 0, WIDTH, HEIGHT);
-
-        game.Render(context, WIDTH, HEIGHT);
-    }
-    
-    Main();
-
-    game.Start();
 }
 
 console.log("app.js Starting up.");
--- js/game.js
+++ js/game.js
@@ -1,35 +1,39 @@
 console.log("Game class being imported.");
 
-function Game(width, height) {
-    var bubble;
+class Game {
+    constructor(width, height) {
+        var bubble;
 
-    this.Start = function() {
-        console.log("Game starting.");
-    }
+        this.Start = function () {
+            console.log("Game starting.");
+        };
 
-    this.Render = function(context) {
-        bubble.Render(context);
-    }
+        this.Render = function (context) {
+            bubble.Render(context);
+        };
 
-    function Setup() {
-        console.log("Game object being constructed.")
-        bubble = new Bubble(Math.floor(width*0.5), Math.floor(height*0.5), 30);
+        function Setup() {
+            console.log("Game object being constructed.");
+            bubble = new Bubble(Math.floor(width * 0.5), Math.floor(height * 0.5), 30);
+        }
+        Setup();
     }
-    Setup();
 }
 
-function Bubble(x, y, size) {
-    var color = '#379af7';
+class Bubble {
+    constructor(x, y, size) {
+        var color = '#379af7';
 
-    this.x = x;
-    this.y = y;
-    this.size = size;
+        this.x = x;
+        this.y = y;
+        this.size = size;
 
-    this.Render = function(context) {
-        context.fillStyle = color;
-        context.beginPath();
-        context.moveTo(this.x, this.y);
-        context.arc(this.x, this.y, this.size, 0, Math.PI*2, true);
-        context.fill();
+        this.Render = function (context) {
+            context.fillStyle = color;
+            context.beginPath();
+            context.moveTo(this.x, this.y);
+            context.arc(this.x, this.y, this.size, 0, Math.PI * 2, true);
+            context.fill();
+        };
     }
 }
