9559fe1c1253dae6f25b4f7f729576450ec9aef9
Smithy01 <>
2021-08-08 11:39:05 -0700

    Add full screen background

--- js/app.js
+++ js/app.js
@@ -1,17 +1,45 @@
 
-function App() {
+function App(canvasId) {
+    var WIDTH, HEIGHT;
+
     var animationLoopTimeInterval;
     var game;
+    var canvas, context;
 
     console.log("App object is being constructed.");
 
     function Main() {
+        SetupCanvas();
+
         game = new Game();
 
         animationLoopTimeInterval = setInterval(AnimationLoop, 500);
     }
     
+    function SetupCanvas() {
+        canvas = document.getElementById(canvasId);
+
+        if (canvas && canvas.getContext) {
+            context = canvas.getContext('2d');
+            console.log('Initialized canvas');
+        }
+
+        window.addEventListener('resize', ResizeCanvas, true);
+        ResizeCanvas();
+    }
+
+    function ResizeCanvas() {
+        WIDTH = window.innerWidth;
+        HEIGHT = window.innerHeight;
+
+        canvas.width = WIDTH;
+        canvas.height = HEIGHT;
+    }
+
     function AnimationLoop() {
+        context.fillStyle = '#c6e0ff';
+        context.fillRect(0, 0, WIDTH, HEIGHT);
+
         game.Render();
     }
     
@@ -21,4 +49,4 @@ function App() {
 }
 
 console.log("app.js Starting up.");
-var app = new App();
+var app = new App('world');
