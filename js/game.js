a05ae7534f8c23aa8ec26e856b0d45e7cbb64b39
Smithy01 <>
2021-08-19 11:20:41 -0700

    nvm, only build on blue cells

--- js/game.js
+++ js/game.js
@@ -438,7 +438,7 @@ class Game {
                 }
     
                 if (b.growthFactor < minBubblePopGrowthFactor) {
-                    if (alreadyBuilt) { 
+                    if (alreadyBuilt || b.virus) { 
                         continue;
                     }
                     
@@ -471,7 +471,7 @@ class Game {
                     //energySpawlLostFactor = 0.9;
                     energyLost = b.growthFactor*0.7;
                 }
-
+                   
                 collectedEnergy += Math.floor(energyLost * 100);
                 
                 
